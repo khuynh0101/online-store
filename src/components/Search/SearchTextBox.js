f378aef6a2e553d00cadb6e9d80d74930181c3ec
khuynh0101 <khuynh0101@gmail.com>
2020-06-26 18:01:11 -0400

    finished with search textbox

--- src/components/Search/SearchTextBox.js
+++ src/components/Search/SearchTextBox.js
@@ -1,55 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import styles from './searchTextBox.module.css';
-import { checkEnterKey } from '../../utils/checkEnterKey';
 import { useHistory } from 'react-router-dom';
+import products from '../../data/products.json';
 
 export const SearchTextBox = () => {
   const history = useHistory();
+  const [suggestion, setSuggestion] = useState([]);
   const [search, setSearch] = useState({
     text: '',
     isSearchActive: false,
   });
-
-  const [suggestion, setSuggestion] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+  const [cursor, setCursor] = useState(-1);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      console.log('This will run after 1 second!');
       if (search.text.length > 1) getSuggestion(search);
-    }, 1000);
+    }, 300);
     return () => clearTimeout(timer);
   }, [search]);
 
+  //consolidate unique keywords from JSON
+  useEffect(() => {
+    let keyWordSet = new Set();
+    products.forEach((product) => {
+      const words = product.keywords.split(' ');
+      words.forEach((word) => {
+        keyWordSet.add(word);
+      });
+    });
+    setKeywords([...keyWordSet]);
+  }, []);
+
+  //returning the keywords that match the user's search term
   const getSuggestion = () => {
-    const suggestions = [search.text];
-    setSuggestion(suggestions);
+    const searchText = search.text.toLowerCase();
+    let searchTerms = new Set();
+    keywords.forEach((keyword) => {
+      const word = keyword.toLowerCase();
+      if (word.includes(searchText)) {
+        searchTerms.add(word);
+      }
+    });
+    const term = [search.text];
+    setSuggestion([...searchTerms]);
   };
 
+  //allow only alpha characters and spaces
   const handleOnChange = (e) => {
-    const searchProps = {
-      text: e.target.value,
-      isSearchActive: true,
-    };
-    setSearch(searchProps);
+    const value = e.target.value.replace(/[^A-Za-z\s]/gi, '');
+    setSearchProp(value, true);
   };
-  const handleOnClick = () => {
+  const handleOnFocus = () => {
     const { text } = search;
-    const searchProps = {
-      text: text,
-      isSearchActive: true,
-    };
-    setSearch(searchProps);
+    setSearchProp(text, true);
   };
-  const handleOnBlur = () => {
-    const searchProps = {
-      text: '',
-      isSearchActive: false,
-    };
-    setSearch(searchProps);
+  const closeSearch = () => {
+    setSearchProp('', false);
     setSuggestion([]);
   };
+
+  const setSearchProp = (text, isSearchActive) => {
+    const searchProps = {
+      text: text,
+      isSearchActive: isSearchActive,
+    };
+    setSearch(searchProps);
+  };
+
   const handleSearchSubmit = () => {
     history.push(`/search/${search.text}`);
+    closeSearch();
+  };
+  const handleSuggestionClick = (suggestion) => {
+    history.push(`/search/${suggestion}`);
+    closeSearch();
+  };
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 38 && cursor > -1) {
+      setCursor(cursor - 1);
+    } else if (event.keyCode === 40 && cursor < suggestion.length - 1) {
+      setCursor(cursor + 1);
+    }
+    if (event.keyCode === 13) {
+      if (cursor > -1) handleSuggestionClick(suggestion[cursor]);
+      else handleSearchSubmit();
+    }
   };
 
   const textBoxActiveClassName = search.isSearchActive ? styles.active : null;
@@ -57,15 +93,17 @@ export const SearchTextBox = () => {
     suggestion.length > 0 ? styles.show : styles.hide;
   return (
     <div className={`${styles.navActionSearch} ${textBoxActiveClassName}`}>
+      {search.isSearchActive && (
+        <div className={styles.hiddenDiv} onClick={closeSearch}></div>
+      )}
       <input
         value={search.text}
         className={styles.inputSearch}
         type='text'
         placeholder='Search'
         onChange={handleOnChange}
-        onClick={handleOnClick}
-        onKeyDown={(event) => checkEnterKey(event, handleSearchSubmit)}
-        onBlur={handleOnBlur}
+        onFocus={handleOnFocus}
+        onKeyDown={handleKeyDown}
       />
       <svg
         className={`${styles.navActionSearchImg} ${styles.svgImg}`}
@@ -78,21 +116,25 @@ export const SearchTextBox = () => {
           d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'
         />
       </svg>
-      {
-        //suggestion.length > 0 && (
+      {suggestion.length > 0 && (
         <div className={`${styles.suggestion} ${suggestionBoxClassName}`}>
-          {suggestion.map((suggestion, index) => {
-            return (
-              <>
-                {' '}
-                <p className={styles.suggestionText}>{suggestion}</p>
-                <p className={styles.suggestionText}>{suggestion}</p>
-              </>
-            );
-          })}
+          <ul>
+            {suggestion.map((suggestion, index) => {
+              const highlightClassName =
+                index === cursor ? styles.suggestionActive : null;
+              return (
+                <li
+                  key={index}
+                  className={`${styles.suggestionText} ${highlightClassName}`}
+                  onClick={(index) => handleSuggestionClick(suggestion)}
+                >
+                  {suggestion}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        //)
-      }
+      )}
     </div>
   );
 };
--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -39,83 +39,81 @@ export const ShoppingCart = () => {
           {
             return (
               product && (
-                <>
-                  <ul key={index} className={styles.shoppingBag}>
-                    <li>
-                      <a
-                        className={`${globalStyles.link} ${styles.linkSvgImg}`}
-                        href='#'
-                        onClick={() => removeItem(index)}
+                <ul key={index} className={styles.shoppingBag}>
+                  <li>
+                    <a
+                      className={`${globalStyles.link} ${styles.linkSvgImg}`}
+                      href='#'
+                      onClick={() => removeItem(index)}
+                    >
+                      <svg
+                        className={styles.svgImg}
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
                       >
-                        <svg
-                          className={styles.svgImg}
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 24 24'
-                        >
-                          <path d='M0 0h24v24H0z' fill='none' />
-                          <path
-                            className={styles.svgAction}
-                            d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li className={styles.imgPlantGradient}>
-                      <img className={styles.imgPlant} src={product.url} />
-                    </li>
-                    <li>
-                      <span className={globalStyles.textMedium}>
-                        {product.name}
-                      </span>
-                    </li>
-                    <li className={styles.itemCounterFlex}>
-                      <a
-                        className={`${globalStyles.link} ${styles.linkSvgImg}`}
-                        href='#'
-                        onClick={() => increaseCount(cartItem.id)}
+                        <path d='M0 0h24v24H0z' fill='none' />
+                        <path
+                          className={styles.svgAction}
+                          d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.imgPlantGradient}>
+                    <img className={styles.imgPlant} src={product.url} />
+                  </li>
+                  <li>
+                    <span className={globalStyles.textMedium}>
+                      {product.name}
+                    </span>
+                  </li>
+                  <li className={styles.itemCounterFlex}>
+                    <a
+                      className={`${globalStyles.link} ${styles.linkSvgImg}`}
+                      href='#'
+                      onClick={() => increaseCount(cartItem.id)}
+                    >
+                      <svg
+                        className={styles.svgImg}
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
                       >
-                        <svg
-                          className={styles.svgImg}
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 24 24'
-                        >
-                          <path d='M0 0h24v24H0z' fill='none' />
-                          <path
-                            className={styles.svgAction}
-                            d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'
-                          />
-                        </svg>
-                      </a>
-                      <div
-                        className={`${globalStyles.textMedium} ${styles.counter}`}
+                        <path d='M0 0h24v24H0z' fill='none' />
+                        <path
+                          className={styles.svgAction}
+                          d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'
+                        />
+                      </svg>
+                    </a>
+                    <div
+                      className={`${globalStyles.textMedium} ${styles.counter}`}
+                    >
+                      <span>{cartItem.numItem}</span>
+                    </div>
+                    <a
+                      className={`${globalStyles.link} ${styles.linkSvgImg}`}
+                      href='#'
+                      onClick={() => decreaseCount(cartItem.id)}
+                    >
+                      <svg
+                        className={styles.svgImg}
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
                       >
-                        <span>{cartItem.numItem}</span>
-                      </div>
-                      <a
-                        className={`${globalStyles.link} ${styles.linkSvgImg}`}
-                        href='#'
-                        onClick={() => decreaseCount(cartItem.id)}
-                      >
-                        <svg
-                          className={styles.svgImg}
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 24 24'
-                        >
-                          <path d='M0 0h24v24H0z' fill='none' />
-                          <path
-                            className={styles.svgAction}
-                            d='M19 13H5v-2h14v2z'
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li className={styles.amount}>
-                      <span className={styles.textMedium}>
-                        ${cartItem.numItem * product.price}
-                      </span>
-                    </li>
-                  </ul>
-                </>
+                        <path d='M0 0h24v24H0z' fill='none' />
+                        <path
+                          className={styles.svgAction}
+                          d='M19 13H5v-2h14v2z'
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.amount}>
+                    <span className={styles.textMedium}>
+                      ${cartItem.numItem * product.price}
+                    </span>
+                  </li>
+                </ul>
               )
             );
           }
